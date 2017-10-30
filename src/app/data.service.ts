9f28216cb8ba65abf9610157da6dbd95978441c2
Dan Wahlin <danwahlin@Dans-MacBook-Pro.local>
2017-10-30 10:10:44 -0700

    Moved interface to own file

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,25 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-
-export interface ICustomer {
-  name: string;
-  height: string;
-  mass: string;
-  hair_color: string;
-  skin_color: string;
-  eye_color: string;
-  birth_year: string;
-  gender: string;
-  homeworld: string;
-  films: string[];
-  species: string[];
-  vehicles: string[];
-  starships: string[];
-  created: string;
-  edited: string;
-  url: string;
-}
+import { ICustomer } from './interfaces';
 
 @Injectable()
 export class DataService {
@@ -31,8 +13,8 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getCustomers() {
-    return this.http.get(this.url)
+  getCustomers() : Observable<ICustomer[]> {
+    return this.http.get<ICustomer[]>(this.url)
                .map(data => {
                   return data['results'];
                });
